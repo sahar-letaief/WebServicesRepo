bea1eb57637928927c133f5096d8aa6e99378218
unknown <sahar.letaief@esprit.tn>
2023-01-31 12:16:35 +0100

    tp1 done

--- src/main/java/jaxrs/ressources/HelloRessource.java
+++ src/main/java/jaxrs/ressources/HelloRessource.java
@@ -2,19 +2,23 @@ package jaxrs.ressources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/greetings")
 public class HelloRessource {
 
     @GET
+    @Produces(MediaType.TEXT_HTML)
     public String sayHello(){
         return "Hello from JAX-RS";
     }
 
-    @GET
-    @Produces
-    public String sayHelloTo(String nom,String prenom){
+   @GET
+   @Produces(MediaType.TEXT_HTML)
+   @Path("{FirstName}/{LastName}")
+    public String sayHelloTo(@PathParam(value="FirstName")String nom,@PathParam(value="LastName")String prenom){
         return "hello from JAX-RS"+" "+nom+" "+prenom;
     }
 }
