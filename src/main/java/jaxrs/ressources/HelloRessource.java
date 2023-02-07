ea6d310d73fb043f2da10425cf4bc2a2fde87263
unknown <sahar.letaief@esprit.tn>
2023-02-07 10:16:03 +0100

    tp0 all done

--- src/main/java/jaxrs/ressources/HelloRessource.java
+++ src/main/java/jaxrs/ressources/HelloRessource.java
@@ -1,24 +1,31 @@
 package jaxrs.ressources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/greetings")
 public class HelloRessource {
 
-    @GET
+   /* @GET
     @Produces(MediaType.TEXT_HTML)
     public String sayHello(){
         return "Hello from JAX-RS";
-    }
+    }*/
 
    @GET
    @Produces(MediaType.TEXT_HTML)
    @Path("{FirstName}/{LastName}")
     public String sayHelloTo(@PathParam(value="FirstName")String nom,@PathParam(value="LastName")String prenom){
         return "hello from JAX-RS"+" "+nom+" "+prenom;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    //@Path("{FirstName}/{LastName}") ce n'est la peine de faire cette ligne à cause de query param
+   // @Path("/byQuery")
+    public String sayHelloTo2(@QueryParam(value="FirstName") String nom, @QueryParam(value="LastName") String prenom){
+        if(nom==null && prenom==null)
+            return "Hello from JAX-RS";
+        return "hello from JAX-RS 2"+" "+nom+" "+prenom;
     }
 }
